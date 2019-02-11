4a1e256df272dad881cdff6935398b06b49e17bf
LaPree1 <elhubba@pointpark.edu>
2019-02-11 11:29:16 -0500

    Updated with Quotes

--- connect4.2ply.js
+++ connect4.2ply.js
@@ -10,11 +10,11 @@ class ConnectFour {
   createGrid() {
      const $board = $(this.selector);
      for (let row = 0; row < this.ROWS; row++) {
-       const $row = $('<div>')
-         .addClass('row');
+       const $row = $("<div>")
+         .addClass("row");
          for(let col = 0; col < this.COLS; col++)
-            const $col = $('<div>')
-            .addClass('col empty')
+            const $col = $("<div>")
+            .addClass("col empty")
             $row.append($col);
           }
        $board.append($row);
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   //Make a grid to overwright buttons
-  const connect4 = new Connect4('#connect4')
+  const connect4 = new Connect4("#connect4")
 
 });
