c810eefdf619826c9ae1f816ea2a619bf17c0862
LaPree1 <elhubba@pointpark.edu>
2019-02-11 15:02:35 -0500

    Update connect4.2ply.js

--- connect4.2ply.js
+++ connect4.2ply.js
@@ -12,8 +12,13 @@ class Connect4 {
      for (let row = 0; row < this.ROWS; row++) {
        const $row = $("<div>")
          .addClass("row");
-        $board.append($row);
+         for(let col = 0; col < this.COLS; col++)
+          const $col = $('div');
+          .addClass('col empty');
+          $row.append($col)
     }
+          $board.append($row);
       console.log($board);
+    }
   }
 }
