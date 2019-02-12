4d1e5813e8ad24466ed1bec19001be506607effa
LaPree1 <elhubba@pointpark.edu>
2019-02-12 09:57:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,2 @@
-$(document).ready(function(){
-  //Make a grid to overwright buttons
-  const Connect4 = new Connect4("#connect4");
-
-});
+    //Make a grid to overwright buttons
+const connect4 = new Connect4('#connect4');
