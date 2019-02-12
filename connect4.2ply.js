84ad5e369cab102af1dd8e3a72e5af075bf91fd0
LaPree1 <elhubba@pointpark.edu>
2019-02-12 09:45:00 -0500

    Replacing with TIC TAC TOE logic

--- connect4.2ply.js
+++ connect4.2ply.js
@@ -1,24 +1,26 @@
 class Connect4 {
-  constructor(selector){
+  constructor(selector) {
     this.ROWS = 6;
     this.COLS = 7;
     this.selector = selector;
     this.createGrid();
+    this.setupEventListeners();
   }
 
   createGrid() {
-     const $board = $(this.selector);
-     console.log($board)
-     for (let row = 0; row < this.ROWS; row++) {
-       const $row = $('<div>')
-         .addClass("row");
-         for(let col = 0; col < this.COLS; col++)
-          const $col = $('div');
-          .addClass('col empty');
-          $row.append($col)
-    }
-          $board.append($row);
-      console.log($board);
+    const $board = $(this.selector);
+    $board.empty();
+    for (let row = 0; row < this.ROWS; row++) {
+      const $row = $('<div>')
+        .addClass('row');
+      for (let col = 0; col < this.COLS; col++) {
+        const $col = $('<div>')
+          .addClass('col empty')
+          .attr('data-col', col)
+          .attr('data-row', row);
+        $row.append($col);
+      }
+      $board.append($row);
     }
   }
-}
+};
