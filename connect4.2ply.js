a05c35503875c79f269b84970c85187e4bd85c77
LaPree1 <elhubba@pointpark.edu>
2019-02-17 10:10:20 -0500

    Adding Grid and Button
    Uploading a table header so we can add long buttons for reset and new game.

--- connect4.2ply.js
+++ connect4.2ply.js
@@ -1,3 +1,24 @@
-$("cell").mouseover(function(){
-  $("cell").css("background-color", "yellow");
-});
+var grid = [
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null]
+];
+
+ var player = "X";
+
+ function clearGrid() {
+  for (var row = 0; row < 6; row++) {
+    for (var col = 0; col < 7; col++) {
+      if (grid[row][col]==0) {
+                document.getElementById("cell"+row+col).style.backgroundColor="#FFFFFF";
+      } else if (grid[row][col]==1) { //1 for yellow
+                document.getElementById("cell"+row+col).style.backgroundColor="#FFFF00";
+      } else if (grid[row][col]==2) { //1 for yellow
+                document.getElementById("cell"+row+col).style.backgroundColor="#FF0000";
+       }
+    }
+  }
+}
