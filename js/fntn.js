2e18041093614161dda84f04a40893ba0f606d2a
LaPree1 <elhubba@pointpark.edu>
2019-02-25 10:05:17 -0500

    ELH - Adding Nav Bar
    Added the navi bar to site for team to relink the pages. I am having trouble with making draggable.

--- js/fntn.js
+++ js/fntn.js
@@ -197,3 +197,18 @@ function dWin() {
     }
     return false;
 }
+
+( function( $ ) {
+$( document ).ready(function() {
+$('#cssmenu').prepend('<div id="menu-button">Menu</div>');
+	$('#cssmenu #menu-button').on('click', function(){
+		var menu = $(this).next('ul');
+		if (menu.hasClass('open')) {
+			menu.removeClass('open');
+		}
+		else {
+			menu.addClass('open');
+		}
+	});
+});
+} )( jQuery );
